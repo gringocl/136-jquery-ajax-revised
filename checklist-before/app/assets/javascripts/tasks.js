a5e058a8e12d04e0f6d1b1797a929101784e3104
Miles Starkenburg <milesstarkenburg@gmail.com>
2014-02-11 00:16:20 -0800

    Ajax Complete

---
+++ checklist-before/app/assets/javascripts/tasks.js
@@ -0,0 +1,15 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+$(function () {
+  $('.edit_task').submitOnCheck();
+});
+
+jQuery.fn.submitOnCheck = function () {
+  this.find('input[type=submit]').remove();
+  this.find('input[type=checkbox]').click(function () {
+    $(this).parent('form').submit();
+  });
+  return this;
+};
+
